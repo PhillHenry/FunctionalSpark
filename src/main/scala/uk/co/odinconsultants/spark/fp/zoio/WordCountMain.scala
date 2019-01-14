1ea0d3b91f987fa1f167b8dc4227563901637998
PhillHenry <phillhenry@gmail.com>
2019-01-14 14:53:55 +0000

    Refactoring and documentation.

--- src/main/scala/uk/co/odinconsultants/spark/fp/zoio/WordCountMain.scala
+++ src/main/scala/uk/co/odinconsultants/spark/fp/zoio/WordCountMain.scala
@@ -5,6 +5,8 @@ import uk.co.odinconsultants.spark.fp.zoio.actions.Count._
 import uk.co.odinconsultants.spark.fp.zoio.actions.Read._
 import uk.co.odinconsultants.spark.fp.zoio.actions.Top._
 import uk.co.odinconsultants.spark.fp.zoio.actions.Words._
+import scalaz._
+import scalaz.Scalaz._
 
 /**
   * Stolen from http://www.stephenzoio.com/creating-composable-data-pipelines-spark/
@@ -17,6 +19,19 @@ object WordCountMain {
     val topFn:        Int => SparkOperation[Map[String, Int]] = topWordsOp(countOp(wordsOp(linesOp)))
     val topWordsMap:  Map[String, Int]                        = topFn(100).run(sc)
 
+//    val x = for {
+//      ls <- linesOp
+//      ws <- wordsOp(ls) // <-- "type mismatch; found   : org.apache.spark.rdd.RDD[String] required: uk.co.odinconsultants.spark.fp.zoio.SparkOperation[org.apache.spark.rdd.RDD[String]]"
+//      cs <- countOp(ws)
+//      ts <- topWordsOp(cs)(10)
+//    } yield ts
+
+    monadTransformers(sc)
+
+    sc.stop()
+  }
+
+  private def monadTransformers(sc: SparkContext) = {
     val x = for {
       ls <- linesT(sc)
       ws <- wordsT(ls)
@@ -25,8 +40,5 @@ object WordCountMain {
     } yield ts
 
     println(x.map(_.mkString("\n")))
-
-    sc.stop()
   }
-
 }
